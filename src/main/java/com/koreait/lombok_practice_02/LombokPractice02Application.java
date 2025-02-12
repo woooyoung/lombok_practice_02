78e2fdafaf85e1b67cf52ab6efde7d53bf3554af
woooyoung <axdsw121@gmail.com>
2025-02-12 12:25:57 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/lombok_practice_02/LombokPractice02Application.java
@@ -0,0 +1,13 @@
+package com.koreait.lombok_practice_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokPractice02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokPractice02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/lombok_practice_02/LombokPractice02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.lombok_practice_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokPractice02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
